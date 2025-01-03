4ef0c4a6c0cd494fe2791dc0c58150b46eab7fa0
dun-x <dungta0321@gmail.com>
2025-01-03 11:16:47 +0700

    Bổ sung update container

--- os_update.sh
+++ os_update.sh
@@ -3,10 +3,18 @@ DOCKER=/usr/bin/docker
 DOCKERCOMPOSE=/usr/local/bin/docker-compose
 APT=/usr/bin/apt
 
+echo "---> Updating system"
 sudo $APT update && sudo $APT upgrade -y && sudo $APT autoremove -y
+echo -e "\n"
 
+echo "---> Updating Container"
+for service in $(docker compose ls --format=json | jq -rc ".[] .ConfigFiles"); do
+    $DOCKER compose --file "$service" pull
+    $DOCKER compose --file "$service" up -d
+done
+$DOCKER image prune -f
+echo -e "\n"
+
+echo "---> Updating Snap Store"
 sudo killall snap-store
 sudo snap refresh snap-store
-
-#$DOCKERCOMPOSE pull && $DOCKERCOMPOSE down && $DOCKERCOMPOSE up -d && $DOCKER system prune -a -f
-
