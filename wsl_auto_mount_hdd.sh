b7e2c6ab2da9168007e698dcec98ac589c6b39b3
dun-x <dungta0321@gmail.com>
2025-08-18 20:59:36 +0700

    Tự động restart synthing

--- wsl_auto_mount_hdd.sh
+++ wsl_auto_mount_hdd.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 /mnt/c/Windows/System32/wsl.exe --mount --vhd "E:\WSL\wsl_ubuntu.vhdx" --partition 2 --type ext4 --name hdd
 sudo mount --bind /mnt/wsl/hdd /home/dunx/Desktop/hdd
+
+sleep 60
+
+sudo systemctl restart syncthing@dunx.service
